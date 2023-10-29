9f08a170b197d15f1c6206af6ad734e6e37d5f2a
unknown <fitriadwiseptiani>
2023-10-29 23:45:26 +0700

    fix code

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"github.com/fitriadwiseptiani/task-5-pbi-btpns-Fitria-Dwi-Septiani/controllers/usercontroller"
+	"github.com/fitriadwiseptiani/task-5-pbi-btpns-Fitria-Dwi-Septiani/models"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default();
+	models.ConnectDatabase()
+
+	r.GET("/users/register", usercontroller.Index)
+	r.GET("", usercontroller.Show)
+	r.POST("", usercontroller.Create)
+	r.PUT("", usercontroller.Update)
+	r.DELETE("", usercontroller.Delete)
+
+	r.Run()
 
 }
